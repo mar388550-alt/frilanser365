f4166ddef06f6d52508fc2a00c0c06043a95fd56
mar388550-alt <frilans13365@gmail.com>
2026-02-26 18:21:32 +0300

    Пересоздал main.py и проверил index.html

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-cd templates
-nano index.html
-++
+from fastapi import FastAPI, WebSocket, Request
+from fastapi.templating import Jinja2Templates
+import json
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def get_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    message_counter = 0
+    try:
+        while True:
+            data = await websocket.receive_text()
+            try:
+                request_data = json.loads(data)
+                user_message = request_data.get("message", "")
+            except:
+                continue
+            message_counter += 1
+            response_data = {
+                "number": message_counter,
+                "message": user_message
+            }
+            await websocket.send_text(json.dumps(response_data))
+    except Exception as e:
+        print(f"WebSocket closed: {e}")
