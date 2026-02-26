1b16637288d76e8461cf14b919d341fc02bfb857
mar388550-alt <mar388550@gmail.com>
2026-02-26 19:49:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
-from fastapi import FastAPI, WebSocket, Request
-from fastapi.templating import Jinja2Templates
-import json
+from fastapi import FastAPI
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def get_index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    message_counter = 0
-    try:
-        while True:
-            data = await websocket.receive_text()
-            try:
-                request_data = json.loads(data)
-                user_message = request_data.get("message", "")
-            except:
-                continue
-            message_counter += 1
-            response_data = {
-                "number": message_counter,
-                "message": user_message
-            }
-            await websocket.send_text(json.dumps(response_data))
-    except Exception as e:
-        print(f"WebSocket closed: {e}")
+async def root():
+    return {"message": "Hello World"}
